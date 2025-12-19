e25de65d999bce079f475549308d9ee966ccc49d
Slowburn75 <16010211011a@gmail.com>
2025-12-19 08:25:34 +0100

    back to default

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    base: '/Goshen/', // 👈 VERY IMPORTANT
     plugins: [tailwindcss()],
 })
